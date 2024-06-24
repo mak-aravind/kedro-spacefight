9076b96af02db055e4b966d48b73f9b1d05c187a
mak-dev-aravind <aravind.m@nextgendi.com>
2024-06-24 15:40:08 +0530

    Add existing project files to Git

---
+++ spaceflights/src/spaceflights/pipelines/data_preprocessing/pipeline.py
@@ -0,0 +1,22 @@
+from kedro.pipeline import Pipeline, node, pipeline
+
+from .nodes import preprocess_companies, preprocess_shuttles
+
+
+def create_pipeline(**kwargs) -> Pipeline:
+    return pipeline(
+        [
+            node(
+                func=preprocess_companies,
+                inputs="companies",
+                outputs="preprocessed_companies",
+                name="preprocess_companies_node",
+            ),
+            node(
+                func=preprocess_shuttles,
+                inputs="shuttles",
+                outputs="preprocessed_shuttles",
+                name="preprocess_shuttles_node",
+            ),
+        ]
+    )
